8e61a11f32d57367a5af9bec1a8d85e5edb9df7f
TUSHAR MAURYA <tusharmaurya002@gmail.com>
2023-09-07 01:37:01 +0530

    Updates

---
+++ precache-manifest.901057b8bb78d4881a841373056ec656.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e4b0ffc5c68dea3d82fe76484bfd2c",
+    "url": "/photogram/index.html"
+  },
+  {
+    "revision": "03b93a542bbc46b1a803",
+    "url": "/photogram/static/css/main.e3bd3a6e.chunk.css"
+  },
+  {
+    "revision": "408da45380f25503967c",
+    "url": "/photogram/static/js/2.b68eb8d3.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/photogram/static/js/2.b68eb8d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03b93a542bbc46b1a803",
+    "url": "/photogram/static/js/main.fa7bf203.chunk.js"
+  },
+  {
+    "revision": "62c4c42f5d3a7ce0ec30",
+    "url": "/photogram/static/js/runtime-main.e106f578.js"
+  }
+]);
